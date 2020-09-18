d25c4d6bbf75619fe2cba1143b86dfeb13eeefa5
Pobx <geidtiphong.s@allrightbox.com>
2020-09-18 14:55:27 +0700

    test CRUD OK

--- Entities/ExampleDemo.cs
+++ Entities/ExampleDemo.cs
@@ -6,13 +6,11 @@ namespace WebApi.Entities {
   [Table ("ExampleDemo")]
   public class ExampleDemo : IEntities {
     [Key]
-    public int Id { get; set; }
+    public int Id { get; set; } = 0;
 
     public string Name { get; set; }
     public string IsActive { get; set; } = "Y";
-    [Computed]
     public string CreatedBy { get; set; } = "Pobx";
-    [Computed]
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     public string UpdatedBy { get; set; } = "Pobx";
     public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
