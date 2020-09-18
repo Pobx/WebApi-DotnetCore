ef1cbc572c3fa1d0626b1ad40d2e8af3e7a8221b
Pobx <geidtiphong.s@allrightbox.com>
2020-09-18 13:45:30 +0700

    add IEntities interface to ExampleDemo.cs

--- Entities/ExampleDemo.cs
+++ Entities/ExampleDemo.cs
@@ -1,8 +1,13 @@
 using System;
 namespace WebApi.Entities {
-  public class ExampleDemo {
-    public int Id { get; set; }
-    public string Name {get;set;}
+  public class ExampleDemo : IEntities {
     
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string IsActive { get; set; } = "Y";
+    public string CreatedBy { get; set; } = "Pobx";
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public string UpdatedBy { get; set; } = "Pobx";
+    public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
   }
 }
